019e22c6799c4672299e1e09087fdba8c463bd34
Teal <pershint@github.com>
2016-11-08 19:10:52 -0500

    Added two more RATDB entry subclasses to read from new REACTORS.ratdb file

--- lib/rdbparse.py
+++ lib/rdbparse.py
@@ -14,7 +14,8 @@ import sys
 basepath = os.path.dirname(__file__)
 dbpath = os.path.abspath(os.path.join(basepath, "..", "db"))
 
-REACTOR_RATDB = 'static/REACTORS_corr.ratdb'
+REACTORC_RATDB = 'static/REACTORS_corr.ratdb'
+REACTOR_RATDB = 'static/REACTORS.ratdb'
 CORECOMP_RATDB = 'static/CoreComps.ratdb'
 NUSPEC_RATDB = 'static/NuSpectraConsts.ratdb'
 
@@ -62,7 +63,7 @@ class ratdbEntry(object):
         self.reacdb_entry = {}
         self.ratdbpath = os.path.abspath(os.path.join(dbpath, filename))
 
-    def showValue(self):
+    def show(self):
         print("RATDB type: " + self.rdb_type)
         print("Version: " + self.ver)
         print("Entry index: " + self.index)
@@ -95,13 +96,15 @@ class ratdbEntry(object):
                     if inlinepieces[0] == "index" and \
                     inlinepieces[1].rstrip("\",\n").lstrip(" \"") == self.index:
                         break
-                else:
-                    print("type was not the first entry in a DB, check your DB entries" + \
-                    "for proper formatting!!!!!")
         verlinepieces = verline.split(":")
         self.ver = verlinepieces[1].rstrip(",\n")
         while True:
             line = str(f.readline())
+            print(line)
+            if line.find('//') != -1:
+                continue
+            if line in ['\n', '\r\n']:
+                continue
             if line.find('}') == -1:
                 linepieces = line.split(":")
                 if linepieces[0] == 'version':
@@ -159,8 +162,8 @@ class CoreComp(ratdbEntry):
         super(CoreComp, self).__init__(self.filename, self.rdb_type, index)
         self.stuff = "things"
 
-    def showValue(self):
-        super(CoreComp,self).showValue() #performs the parent method showValue()
+    def show(self):
+        super(CoreComp,self).show() #performs the parent method show()
         print("Isotope composition: " + str(self.composition))
 
     def parseMisc(self):
@@ -178,75 +181,161 @@ class CoreComp(ratdbEntry):
         super(CoreComp, self).buildReacdbEntry()
         self.reacdb_entry["Core Composition (U238, U235, Pu239, Pu241)"] = self.composition
 
-class NuCoeff(ratdbEntry):
+class Reactor_Spectrum(ratdbEntry):
     def __init__(self,index):
-        self.rdb_type = "SPECTRACOEFF"
-        self.filename = NUSPEC_RATDB
-        self.U235 = []
-        self.U238 = []
-        self.Pu239 = []
-        self.Pu241 = []
-        super(NuCoeff, self).__init__(self.filename, self.rdb_type, index)
+        self.rdb_type = "REACTOR_SPECTRUM"
+        self.filename = REACTOR_RATDB
+        self.emin = 'none'
+        self.emax = 'none'
+        self.spectrum_type = 'none'
+        self.spec_e = []
+        self.spec_flux = []
+        self.flux_norm = 'none'
+        self.param_isotope = []
+        self.param_composition = []
+        super(Reactor_Spectrum, self).__init__(self.filename, self.rdb_type, index)
 
-    def showValue(self):
-        super(NuCoeff,self).showValue() #performs the parent method showValue()
-        print("U235 parameters: " + str(self.U235))
-        print("U238 parameters: " + str(self.U238))
-        print("Pu239 parameters: " + str(self.Pu239))
-        print("Pu241 parameters: " + str(self.Pu241))
-
+    def show(self):
+        super(Reactor_Spectrum, self).show()
+        print("Minimum valid energy for spectrum: " + str(self.emin))
+        print("Maximum valid energy for spectrum: " + str(self.emax))
+        print("Spectrum type: " + str(self.spectrum_type))
+        print("Energy values (x-axis for spectrum): " + str(self.spec_e))
+        print("Flux values (y-axis for spectrum): " + str(self.spec_flux))
+        print("Flux normalization factor: " + str(self.flux_norm))
+        print("Isotopes used to build spectrum: " + str(self.param_isotope))
+        print("% of each isotope (235U, 238U, 239Pu, 241Pu): " + str(self.param_composition))
 
     def parseMisc(self):
-        U235_str = self.misc['U235']
-        U235_str = U235_str.lstrip("\' [").rstrip("],\n\'")
-        U235_str = U235_str.split(",")
-        U235_arr = []
-        for entry in U235_str:
-            entry = float(entry)
-            U235_arr.append(entry)
-        self.U235 = U235_arr
-        U238_str = self.misc['U238']
-        U238_str = U238_str.lstrip("\' [").rstrip("],\n\'")
-        U238_str = U238_str.split(",")
-        U238_arr = []
-        for entry in U238_str:
-            entry = float(entry)
-            U238_arr.append(entry)
-        self.U238 = U238_arr
-        Pu239_str = self.misc['Pu239']
-        Pu239_str = Pu239_str.lstrip("\' [").rstrip("],\n\'")
-        Pu239_str = Pu239_str.split(",")
-        Pu239_arr = []
-        for entry in Pu239_str:
-            entry = float(entry)
-            Pu239_arr.append(entry)
-        self.Pu239 = Pu239_arr
-        Pu241_str = self.misc['Pu241']
-        Pu241_str = Pu241_str.lstrip("\' [").rstrip("],\n\'")
-        Pu241_str = Pu241_str.split(",")
-        Pu241_arr = []
-        for entry in Pu241_str:
-            entry = float(entry)
-            Pu241_arr.append(entry)
-        self.Pu241 = Pu241_arr
-      
+        spece_vals = self.misc['spec_e']
+        spece_vals = spece_vals.lstrip("\' [").rstrip("],\n\'")
+        spece_vals = spece_vals.split(",")
+        spece_vals_arr = []
+        for entry in spece_vals:
+            value = float(entry)
+            spece_vals_arr.append(value)
+        self.spec_e = spece_vals_arr
+
+        specf_vals = self.misc['spec_flux']
+        specf_vals = specf_vals.lstrip("\' [").rstrip("],\n\'")
+        specf_vals = specf_vals.split(",")
+        specf_vals_arr = []
+        for entry in specf_vals:
+            value = float(entry)
+            specf_vals_arr.append(value)
+        self.spec_flux = specf_vals_arr
+
+        emin = self.misc['emin']
+        emin = emin.lstrip("\' ").rstrip(",\n\'")
+        self.emin = emin
+
+        emax = self.misc['emax']
+        emax = emax.lstrip("\' ").rstrip(",\n\'")
+        self.emax = emax
+       
+        fnorm = self.misc['flux_norm']
+        fnorm = fnorm.lstrip("\' ").rstrip(",\n\'")
+        self.flux_norm = fnorm
+        
+        spectype = self.misc['spectrum_type']
+        spectype = spectype.lstrip("\' \"").rstrip("\",\n\'")
+        self.spectrum_type = spectype
+        
+        paramiso_vals = self.misc['param_isotope']
+        paramiso_vals = paramiso_vals.lstrip("\' [").rstrip("],\n\'")
+        paramiso_vals = paramiso_vals.split(",")
+        paramiso_vals_arr = []
+        for entry in paramiso_vals:
+            entry = entry.lstrip(" \"").rstrip("\"")
+            paramiso_vals_arr.append(entry)
+        self.param_isotope = paramiso_vals_arr
+        
+        paramcomp_vals = self.misc['param_composition']
+        paramcomp_vals = paramcomp_vals.lstrip("\' [").rstrip("],\n\'")
+        paramcomp_vals = paramcomp_vals.split(",")
+        paramcomp_vals_arr = []
+        for entry in paramcomp_vals:
+            value = float(entry)
+            paramcomp_vals_arr.append(value)
+        self.param_composition = paramcomp_vals_arr
+
     def buildreacdbEntry(self):
-        super(NuCoeff, self).buildReacdbEntry()
-        self.reacdb_entry["U235 parameter values [a1,a2,a3,a4,a5,a6]"] = self.U235
-        self.reacdb_entry["U238 parameter values [a1,a2,a3,a4,a5,a6]"] = self.U238
-        self.reacdb_entry["Pu239 parameter values [a1,a2,a3,a4,a5,a6]"] = self.Pu239
-        self.reacdb_entry["Pu241 parameter values [a1,a2,a3,a4,a5,a6]"] = self.Pu241
+        super(Reactor_Spectrum, self).buildReacdbEntry()
+        self.reacdb_entry["Spectrum energy (x-axis)"] = self.spec_e
+        self.reacdb_entry["Spectrum flux (y-axis)"] = self.spec_flux
+        self.reacdb_entry["Minimum valid energy for spectrum"] = self.emin
+        self.reacdb_entry["Maximum valid energy for spectrum"] = self.emax
+        self.reacdb_entry["Normalization factor for spectrum"] = self.flux_norm
+        self.reacdb_entry["Spectrum type"] = self.spectrum_type
+        self.reacdb_entry["Isotopes used to generate spectrum"] = self.param_isotope
+        self.reacdb_entry["% of each isotope composing reactor [235U, 238U, 239Pu, 241Pu]"] = self.param_composition
+
+class Reactor_Isotope_Info(ratdbEntry):
+    def __init__(self,index):
+        self.rdb_type = "REACTOR_ISOTOPE_INFO"
+        self.filename = REACTOR_RATDB
+        self.nuperfission = 'none'
+        self.nuperfission_err = 'none'
+        self.Eperfission = 'none'
+        self.Eperfission_err = 'none'
+        self.spec_type = 'none'
+        self.poly_coeff = []
+        super(Reactor_Isotope_Info, self).__init__(self.filename, self.rdb_type, index)
+
+    def show(self):
+        super(Reactor_Isotope_Info,self).show() #performs the parent method show()
+        print("neutrinos per fission: " + str(self.nuperfission))
+        print("error on neutrinos per fission: " + str(self.nuperfission_err))
+        print("Energy released per fission: " + str(self.Eperfission))
+        print("error on energy released per fission: " + str(self.Eperfission_err))
+        print("Type used to characterize spectrum: " + str(self.spec_type))
+        print("Polynomial coefficients: " + str(self.poly_coeff))
+
+    def parseMisc(self):
+        PolyCoeffs = self.misc['poly_coeff']
+        PolyCoeffs = PolyCoeffs.lstrip("\' [").rstrip("],\n\'")
+        PolyCoeffs = PolyCoeffs.split(",")
+        PolyCoeffs_arr = []
+        print(PolyCoeffs)
+        for entry in PolyCoeffs:
+            value = float(entry)
+            PolyCoeffs_arr.append(value)
+        self.poly_coeff = PolyCoeffs_arr
+        nnuf = self.misc['n_nu_fission']
+        nnuf = nnuf.lstrip("\' ").rstrip(",\n\'")
+        self.nuperfission = nnuf
+        nnuf_err = self.misc['n_nu_fission_err']
+        nnuf_err = nnuf_err.lstrip("\' ").rstrip(",\n\'")
+        self.nuperfission_err = nnuf_err
+        epf = self.misc['e_per_fission']
+        epf = epf.lstrip("\' ").rstrip(",\n\'")
+        self.Eperfission = epf
+        epf_err = self.misc['e_per_fission_err']
+        epf_err = epf_err.lstrip("\' ").rstrip(",\n\'")
+        self.Eperfission_err = epf_err
+        spectype = self.misc['spec_type']
+        spectype = spectype.lstrip("\' \"").rstrip("\",\n\'")
+        self.spec_type = spectype
+
+    def buildreacdbEntry(self):
+        super(Reactor_Isotope_Info, self).buildReacdbEntry()
+        self.reacdb_entry["Parameter values [a1,a2,a3,a4,a5,a6]"] = self.poly_coeff
+        self.reacdb_entry["Number of neutrinos per fission"] = self.nuperfission
+        self.reacdb_entry["Error on # neutrinos per fission"] = self.nuperfission_err
+        self.reacdb_entry["Energy released per fission (MeV)"] = self.Eperfission
+        self.reacdb_entry["Error on E released per fission (MeV)"] = self.Eperfission_err
+        self.reacdb_entry["Type used to characterize spectrum"] = self.spec_type
  
 class ReactorSpecs(ratdbEntry):
     def __init__(self,index):
         self.rdb_type = "REACTORS"
-        self.filename = REACTOR_RATDB
+        self.filename = REACTORC_RATDB
         self.power_therm = 'unknown'
         self.longlat = ['unknown', 'unknown']
         super(ReactorSpecs, self).__init__(self.filename, self.rdb_type, index)
 
-    def showValue(self):
-        super(ReactorSpecs,self).showValue() #performs the parent method showValue()
+    def show(self):
+        super(ReactorSpecs,self).show() #performs the parent method show()
         print("Reactor Thermal Output (MW): " + str(self.power_therm))
         print("Reactor position [long, lat] (degrees): " + str(self.longlat))
 
