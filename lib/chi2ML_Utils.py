f7be82ce41cc20305569c5366b825d4a0d65ff35
Teal <pershint@github.com>
2017-02-20 21:37:39 -0500

    Finished adding maximum likelihood best fitter

--- lib/chi2ML_Utils.py
+++ lib/chi2ML_Utils.py
@@ -37,9 +37,10 @@ def getExpt_wstats(oscParams, All_unosc_spectra,energy_array, numBins):
 #spectra used to generate the event histograms, Event histogram with statistical
 #fluctuation, Event histogram without statistical fluctuation
 class ExperimentChi2(object):
-    def __init__(self, unosc_spectra, energy_array,Stat_EventHist):
+    def __init__(self, unosc_spectra, energy_array,Stat_EventHist,NoStat_EventHist):
         self.unosc_spectra = unosc_spectra
         self.Stat_EventHist = Stat_EventHist
+        self.NoStat_EventHist = NoStat_EventHist
         self.energy_array = energy_array
     def __call__(self, sst, dms):
         print("OSC PARAMS FED IN: " + str([dms,sst]))
@@ -62,7 +63,7 @@ def GetChi2dmsFixed(unosc_spectra, oscParams, sst_array, energy_array,NUMBINS):
     '''
     EventHist_wstats, EventHist = getExpt_wstats(oscParams, unosc_spectra, \
             energy_array,NUMBINS)
-    chi2 = ExperimentChi2(unosc_spectra,energy_array,EventHist_wstats)
+    chi2 = ExperimentChi2(unosc_spectra,energy_array,EventHist_wstats,EventHist)
     chi2_results = []
     for sst in sst_array:
         chi2_results.append(chi2(sst, oscParams[0]))
@@ -83,7 +84,7 @@ def Getchi2StatSpread(num_experiments, unosc_spectra,oscParams,energy_array, NUM
         EventHist_wstats, EventHist = getExpt_wstats(oscParams,unosc_spectra, \
                 energy_array,NUMBINS)
         print("CHISQUARE BEING CALCULATED NOW FOR A RANDOM EXPERIMENT...")
-        chi2 = ExperimentChi2(unosc_spectra,energy_array, EventHist_wstats)
+        chi2 = ExperimentChi2(unosc_spectra,energy_array, EventHist_wstats,EventHist)
         im.describe(chi2)
         m = im.Minuit(chi2, limit_dms = (1e-07, 1e-03), limit_sst=(0.0,1.0), sst = (oscParams[1]), dms = oscParams[0])
         m.migrad()
@@ -103,9 +104,10 @@ def Getchi2StatSpread(num_experiments, unosc_spectra,oscParams,energy_array, NUM
 ## --------------------------------------------------------------------- ##
 
 class ExperimentNegML(object):
-    def __init__(self, unosc_spectra, energy_array,Stat_EventHist):
+    def __init__(self, unosc_spectra, energy_array,Stat_EventHist,NoStat_EventHist):
         self.unosc_spectra = unosc_spectra
         self.Stat_EventHist = Stat_EventHist
+        self.NoStat_EventHist = NoStat_EventHist
         self.energy_array = energy_array
     def __call__(self, sst, dms):
         print("OSC PARAMS FED IN: " + str([dms,sst]))
@@ -139,7 +141,7 @@ def GetNegMLStatSpread(num_experiments, unosc_spectra,oscParams,energy_array, NU
         EventHist_wstats, EventHist = getExpt_wstats(oscParams,unosc_spectra, \
                 energy_array,NUMBINS)
         print("CHISQUARE BEING CALCULATED NOW FOR A RANDOM EXPERIMENT...")
-        negML = ExperimentNegML(unosc_spectra,energy_array, EventHist_wstats)
+        negML = ExperimentNegML(unosc_spectra,energy_array, EventHist_wstats,EventHist)
         im.describe(negML)
         m = im.Minuit(negML, limit_dms = (1e-07, 1e-03), limit_sst=(0.0,1.0), sst = (oscParams[1]), dms = oscParams[0])
         m.migrad()
