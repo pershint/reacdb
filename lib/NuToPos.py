fb06933c46472999b860bc72bb243d3db2679697
Teal <pershint@github.com>
2017-04-12 20:14:30 -0400

    Did some testing, NuToPos seems to work!

--- lib/NuToPos.py
+++ lib/NuToPos.py
@@ -13,24 +13,33 @@ class NuToPosConverter(object):
         self._m_e = 0.511 #m_electron in MeV
         self._m_n = 939.57 #in MeV
 
-    def convert0(self):
+    def _convert_0ord(self):
         #zeroth order conversion to positron momentum
         p_p0 = np.sqrt((self.E_nus - self._delta)**2 - (self._m_e)**2)
         return p_p0
 
-    def convert1(self,p_pos0):
+    def _convert_1ord(self,p_pos0):
         #first order conversion to positron momentum
         #source: Vogel, Beacom, "The angular distribution of the 
         #reaction nu_e + p -> pos + n". arXiv:hep-ph/9903554v1, Apr 1999
         Ee0 = self.E_nus - self._delta
         ve0 = p_pos0/(self.E_nus - self._delta)
-        exp_cos = -0.034 * ve0 + 2.4 * (self.E_nus / self.m_n)
+        exp_cos = -0.034 * ve0 + 2.4 * (self.E_nus / self._m_n)
         y = (self._delta**2 - self._m_e**2)/2
-        Ee1 = Ee0(1 - (self.E_nus/self._m_n)*(1 - ve0*exp_cos)) - ((y**2)/self._m_n)
+        Ee1 = Ee0*(1 - (self.E_nus/self._m_n)*(1 - ve0*exp_cos)) - ((y**2)/self._m_n)
         pe1 = np.sqrt(Ee1**2 - self._m_e**2)
         return pe1
 
-    def addAnnihilE(self, pe1):
+    def convert(self):
+        p_p0 = self._convert_0ord()
+        p_p1 = self._convert_1ord(p_p0)
+        return p_p1
+
+    def addAnnihilE(self, pos_mom):
+        '''
+        Takes an array of positron momentums and adds to each the energy
+        released in a positron-electron annihilation.
+        '''
         E_ann = self._m_e * 2
         return pe1 + E_ann
 
