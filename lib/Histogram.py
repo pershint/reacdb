Fixed the insane scaling of the positron energy

lib/Histogram.py
@@ -1,3 +1,4 @@
+import time
 import rdbparse as rp
 import SNOdist as sd
 import NuSpectrum as ns
@@ -23,11 +24,154 @@ class Histogram(object):
         #initialize value that tells you the bin width in x-axis units
         self.bin_width = self.bin_rights[0] - self.bin_lefts[0]
 
+class dNdE_Hist_new(Histogram):
+    def __init__(self, dNdE, numbins, hmin, hmax):
+        self.spectrum = dNdE.dNdE
+        self.x_axis = dNdE.Pos_Energy_Array
+        self._padding = 0.01
+        if (hmin is not None) or (hmin is not None):
+            self.hmin = float(hmin)
+            self.hmax = float(hmax)
+        else:
+            self.hmin = np.min(self.spectrum)
+            self.hmax = np.max(self.spectrum) + self._padding
+        self.numbins = numbins
+        self.bincheck()
+
+        #Initialize arrays that hold bin end locations
+        self.bin_lefts, self.bin_rights, self.bin_centers =  \
+                self.Bin_spectrum()
+        print("ENERGYRANGE " + str(self.x_axis))
+        self.bin_values = self.fill_binvalues()
+
+        super(dNdE_Hist_new, self).__init__(self.bin_values, self.bin_centers, \
+                self.bin_lefts, self.bin_rights)
+
+    def bincheck(self):
+        '''
+        Function that checks the number of bins is not greater than the
+        number of spectrum entries.
+        '''
+        if self.numbins > len(self.spectrum):
+            print("ERROR: CANNOT USE MORE BINS THAN SPECTRUM ARRAY ENTRIES." +\
+                    "SETTING NUMBER OF BINS TO NUMBER OF SPECTRUM VALUES.")
+            self.numbins = self.spectrum
+
+       
+    def fill_binvalues(self):
+        bin_values = []
+        cbin = 0
+        endofvalrange = False
+        while cbin < self.numbins:
+            if endofvalrange == True:
+                bin_values.append(0.0)
+                cbin+=1
+                break
+            thisbinsavg = []
+            for j,val in enumerate(self.x_axis):
+                if cbin == 0:
+                    if (val >= self.bin_lefts[cbin] and val <= self.bin_rights[cbin]):
+                        thisbinsavg.append(self.spectrum[j])
+                        continue
+                    if val > self.bin_rights[cbin]:
+                        binwidth = self.bin_rights[cbin] - self.bin_lefts[cbin]
+                        bin_values.append(np.mean(thisbinsavg) * binwidth)
+                        cbin+=1
+                        break
+                else:
+                     if (val > self.bin_lefts[cbin] and val <= self.bin_rights[cbin]):
+                         thisbinsavg.append(self.spectrum[j])
+                         if j == len(self.x_axis) - 1:
+                             #Reached the end of our spectrum axes
+                             binwidth = self.bin_rights[cbin] - self.bin_lefts[cbin]
+                             bin_values.append(np.mean(thisbinsavg) * binwidth)
+                             cbin+=1
+                             endofvalrange = True
+                             break
+                         continue
+                     #check if this spectrum value is past the current bin edge
+                     if val > self.bin_rights[cbin]:
+                         binwidth = self.bin_rights[cbin] - self.bin_lefts[cbin]
+                         bin_values.append(np.mean(thisbinsavg) * binwidth)
+                         cbin+=1
+                         break
+        bin_values = np.array(bin_values)
+        return bin_values
+
+    def Bin_spectrum(self):
+        """
+        Takes the input spectrum and corresponding energy array and
+        defines the bin ranges for the histogram. Note: if the spectrum
+        has 100 bins, and num_combine == 10, the final number of bins will be
+        10 with the average of 10 bins in each.
+        """
+        cbin = 0
+        bls, brs, bcs = [],[],[]
+        #set up your bins
+        while cbin < self.numbins: 
+            bls.append(self.hmin + (((self.hmax - self.hmin)/self.numbins) * cbin))
+            brs.append(self.hmin + (((self.hmax - self.hmin)/self.numbins) * (cbin+1)))
+            bcs.append(bls[cbin] + ((brs[cbin]-bls[cbin]/2)))
+            cbin += 1
+        return bls, brs, bcs  
+  
+  #This needs work.  The issue is when you try to make your bin lefts and rights,
+#The decimal number gets offset and you'll get one too many bins.  Quick fix: strip
+#off the extra bin that doesn't match up with the requested number of bins.
+class Event_Hist(Histogram):
+    def __init__(self, events, numbins, hmin, hmax):
+        self._padding = 0.01
+        self.events = events
+        if (hmin is not None) or (hmin is not None):
+            self.hmin = float(hmin)
+            self.hmax = float(hmax)
+        else:
+            self.hmin = np.min(events)
+            self.hmax = np.max(events) + self._padding
+        self.numbins = numbins
+        self.bin_width = (self.hmax - self.hmin) / float(self.numbins)
+
+        #Initialize arrays that hold bin end locations
+        self.bin_lefts = np.arange(self.hmin, \
+                self.hmax,self.bin_width)
+        self.bin_rights = np.arange((self.hmin + self.bin_width), \
+                (self.hmax + self.bin_width), self.bin_width)
+        #Issues with representing hmin and hmax related to representing the float
+        #In binary.  Strip off extra bins that were made in error.
+        self.binstrip()
+        self.bin_centers = (self.bin_lefts + ((self.bin_rights - self.bin_lefts) / 2.0))
+
+        #initialize array to hold the bin's value
+        self.bin_values = []
+        #initialize value that tells you the bin width in x-axis units
+        self.fill_binvalues()
+        self.bin_values = np.array(self.bin_values)
+
+        super(Event_Hist, self).__init__(self.bin_values, self.bin_centers, \
+                self.bin_lefts, self.bin_rights)
+
+    def binstrip(self):
+        if len(self.bin_rights) > len(self.bin_lefts):
+            self.bin_rights = np.delete(self.bin_rights,(len(self.bin_rights)-1))
+        elif len(self.bin_rights) < len(self.bin_lefts):
+            self.bin_lefts = np.delete(self.bin_lefts,(len(self.bin_lefts)-1))
+
+
+    def fill_binvalues(self):
+        bin_vals = []
+        nbin = 0
+        while nbin < self.numbins:
+            bin_vals.append(len(np.where((self.events >= self.bin_lefts[nbin]) & 
+                (self.events < self.bin_rights[nbin]))[0])) #[0]? np.where is dumb
+            print(bin_vals[nbin])
+            print(nbin)
+            nbin+=1
+        self.bin_values = bin_vals
 #Turns a dNdE function into a binned distribution
 class dNdE_Hist(Histogram):
     def __init__(self, dNdE, numbins):
         self.spectrum = dNdE.dNdE
-        self.x_axis = dNdE.Energy_Array
+        self.x_axis = dNdE.Pos_Energy_Array
         self.numbins = numbins
         self.specvals_perbin = len(self.spectrum) / self.numbins
         self.bincheck()

lib/NuSpectrum.py
@@ -1,10 +1,9 @@
-#g!
-
 import sys
 import ctypes as ct
 import os.path
 import config.config as c
 
+import NuToPos as ntp
 import rdbparse as rp
 import playDarts as pd
 import GetNRCDailyInfo as nrc
@@ -45,7 +44,7 @@ A2 = 0.02018
 A3 = -0.001953
 
 #Takes in an array of UnoscSpecGen classes (assume all have same energy points on y-axis)
-#And returns the dNdE function that results from them
+#And returns the dNdE function that results from them (neutrino energy)
 def build_Theory_dNdE(unosc_spectra,oscParams):
     energy_array = unosc_spectra[0].energy_array
     Total_PerfectSpectra = np.zeros(len(energy_array))
@@ -362,7 +361,7 @@ class Osc_CoreSysGen(object):
 #with the spectrum's core must already be factored into the spectrum.
 class dNdE(object):
     def __init__(self,Energy_Array,Spectrum):
-        self.Energy_Array = Energy_Array
+        self.Nu_Energy_Array = Energy_Array
         self.Spectrum = Spectrum
 
         self.Array_Check()
@@ -372,13 +371,20 @@ class dNdE(object):
         self.dNdE = []
         self.evaldNdE()
 
+        self.Pos_Energy_Array = self.getPositronEnergyArr()
+
+    def getPositronEnergyArr(self):
+        Converter = ntp.NuToPosConverter()
+        pos_energies = Converter.ConvertToPositron(self.Nu_Energy_Array)
+        return pos_energies
+
     def evaldNdE(self):
         self.dNdE = [] #Remove any previous values in dNdE
         self.dNdE = c.EFFICIENCY * c.NP * \
-            self.XC(self.Energy_Array) * self.Spectrum
+            self.XC(self.Nu_Energy_Array) * self.Spectrum
 
     def Array_Check(self):
-        if len(self.Energy_Array) != len(self.Spectrum):
+        if len(self.Nu_Energy_Array) != len(self.Spectrum):
             raise UserWarning("WARNING: Energy array length does not equal" + \
                     "length of spectrum given.  Check your entries.")
 
@@ -400,6 +406,8 @@ class dNdE(object):
     def smear(self):
         '''
         Smears the current dNdE by the defined detector resolution.
+        NOTE: Smearing only valid when dNdE is now as a function of
+        Positron energy.
         '''
         if self.resolution is None:
             print("Define your resolution first!  Exiting...")
@@ -418,8 +426,8 @@ class dNdE(object):
     
         #cast inputs as numpy arrays (just in case they were a list)
         spec = np.array(self.dNdE)
-        e_arr = np.array(self.Energy_Array)
-    
+        e_arr = np.array(self.Pos_Energy_Array)
+
         #use the np.array.ctypes function call to cast data as needed for ctypes
         spec_in = spec.ctypes.data_as(pdub)
         e_arr_in = e_arr.ctypes.data_as(pdub)
@@ -432,7 +440,5 @@ class dNdE(object):
     
         #go back to a python-usable numpy array
         smearedspec = np.array(np.fromiter(indata, dtype=np.float64, count=numpoints))
-        #normalize to binwidth
-        smearedspec = smearedspec * (self.Energy_Array[1]-self.Energy_Array[0])
         self.dNdE = smearedspec
 

lib/chi2ML_Utils.py
@@ -10,6 +10,7 @@ import NuSpectrum as ns
 import Histogram as h
 import playDarts as pd
 
+
 #builds the chi-squared between the expected SNO+ spectrum with
 #systematics included and an oscillated spectrum with no systematics
 #that is oscillated with a (sine-squared theta12) and b
@@ -21,13 +22,13 @@ import playDarts as pd
 #Fluctuation generated using the PlayDarts library.
 def getExpt_wstats(oscParams, All_unosc_spectra):
     VarieddNdE = ns.build_Theory_dNdE_wCoreSys(All_unosc_spectra, oscParams)
+    #integrate neutrino dNdE to get avg. # neutrinos for experiment
     if "DETECTOR_RESP" in c.SYSTEMATICS:
         VarieddNdE.setResolution(c.RESOLUTION)
         VarieddNdE.smear()
-    Varied_EventHist = h.dNdE_Hist(VarieddNdE, c.NUMBINS)
-    events_in_experiment = sum(Varied_EventHist.bin_values)
-    print("EVTS PER YEAR: " + str(events_per_year))
-    if events_per_year > 15:
+    Varied_EventHist = h.dNdE_Hist_new(VarieddNdE, c.NUMBINS,c.HMIN,c.HMAX)
+    print("EVTS PER YEAR: " + str(events_in_experiment))
+    if events_in_experiment > 15:
         n = int(pd.RandShoot(events_in_experiment, np.sqrt(events_in_experiment),1))
     else:
         n = pd.RandShoot_p(events_in_experiment,1)
@@ -38,16 +39,17 @@ def getExpt_wstats(oscParams, All_unosc_spectra):
 def getExpt_wstats_PosE(oscParams, All_unosc_spectra):
     VarieddNdE = ns.build_Theory_dNdE_wCoreSys(All_unosc_spectra, oscParams)
     #DO A ROUGH INTEGRATE TO GET EVENTS IN EXPERIMENT TIME
-    #events_in_experiment
-    nu_energies = pd.playDarts(events_in_experiment,Perfect_dNdE.dNdE,c.ENERGY_ARRAY)
+    TheoryEventNum = int(pd.RoughIntegrate(VarieddNdE.dNdE,VarieddNdE.Nu_Energy_Array))
+    events_in_experiment = int(pd.RandShoot(TheoryEventNum, np.sqrt(TheoryEventNum),1))
+    nu_energies = pd.playDarts(events_in_experiment,Perfect_dNdE.dNdE,c.NU_ENERGY_ARRAY)
     print("NU ENERGIES: " + str(nu_energies))
     NuPosConverter = ntp.NuToPosConverter()
     pos_energies = NuPosConverter.ConvertToPositron(nu_energies)
     if "DETECTOR_RESP" in c.SYSTEMATICS:
         pos_energies = NuPosConverter.Smear(pos_energies,c.RESOLUTION)
-    #FIXME: THE ENERGY_ARRAY IS STILL FOR NEUTRINOS.  WE NEED TO RE-CALCULATE FOR
+    #FIXME: THE NU_ENERGY_ARRAY IS STILL FOR NEUTRINOS.  WE NEED TO RE-CALCULATE FOR
     #POSITRON WINDOW NOW!!!!
-    Stat_EventHist = h.Event_Hist(pos_energies,c.NUMBINS,c.ENERGY_ARRAY[0],c.ENERGY_ARRAY[len(c.ENERGY_ARRAY) -1])
+    Stat_EventHist = h.Event_Hist(pos_energies,c.NUMBINS,c.HMIN,c.HMAX)
     return Stat_EventHist
 
 ## ------------ BEGIN FUNCTIONS/CLASSES FOR CHI-SQUARED TESTS ---------- ##

lib/config/config.py
@@ -16,12 +16,13 @@ ISOTOPES = ['235U', '238U', '239Pu', '241Pu']
 
 #Defines our energy range for our spectra/dNdE and the resoultion
 #unites are in MeV.
-ENERGY_ARRAY = np.arange(1.82,9.02,0.01)
+NU_ENERGY_ARRAY = np.arange(1.82,9.02,0.011)
 
 #Set the number of bins that your histograms will be binned into
 #Want len(ENERGY_ARRAY) to be divisible by NUMBINS.
 NUMBINS = 30
-
+HMIN = 1.02
+HMAX = 8.82
 
 RUNTIME = 8760*5   #Run years in hours
 

main.py
@@ -78,7 +78,7 @@ def getBruceSpectra():
     BruceDetails = rp.ReactorDetails("BRUCE")
     BruceStatus = rp.ReactorStatus("BRUCE")
     BruceUnoscSpecGen = ns.UnoscSpecGen(BruceDetails,BruceStatus,Isotope_Information, \
-            c.ENERGY_ARRAY,c.RUNTIME)
+            c.NU_ENERGY_ARRAY,c.RUNTIME)
     BruceOscSpecGen = ns.OscSysGen(BruceUnoscSpecGen,oscParams)
     for CORENUM in np.arange(1,BruceUnoscSpecGen.no_cores+1):
         print("GRAPHING OSC SPECTRA FOR " + str(CORENUM) + " NOW...")
@@ -141,7 +141,7 @@ def build_unoscSpectra(List):
         ReacDetails = rp.ReactorDetails(reactor)
         ReacStatus = rp.ReactorStatus(reactor)
         ReacUnoscSpecGen = ns.UnoscSpecGen(ReacDetails,ReacStatus, \
-                Isotope_Information, c.ENERGY_ARRAY,c.RUNTIME)
+                Isotope_Information, c.NU_ENERGY_ARRAY,c.RUNTIME)
         unosc_spectra.append(ReacUnoscSpecGen)
     return unosc_spectra
 
@@ -168,38 +168,34 @@ if __name__ == '__main__':
 
     #Debugging the new Event builder function
     if DEBUG == True:
-        Perfect_dNdE = ns.build_Theory_dNdE(unosc_spectra,oscParams)
+        NoCoreSys_dNdE = ns.build_Theory_dNdE(unosc_spectra,oscParams)
         if "DETECTOR_RESP" in c.SYSTEMATICS:
             #FIXME: Don't want resolution hard-coded... 
-            Perfect_dNdE.setResolution(0.075)
-            Perfect_dNdE.smear()
-        nu_energies = pd.playDarts(10000,Perfect_dNdE.dNdE,c.ENERGY_ARRAY)
-        print("NU ENERGIES: " + str(nu_energies))
-        htest = h.Event_Hist(nu_energies,c.NUMBINS,c.ENERGY_ARRAY[0],c.ENERGY_ARRAY[len(c.ENERGY_ARRAY) -1])
-        print("HIST BINNING FROM h.Event_Hist")
-        print(htest.bin_lefts[0])
-        print(htest.bin_rights[len(htest.bin_rights)-1])
+            NoCoreSys_dNdE.setResolution(c.RESOLUTION)
+            NoCoreSys_dNdE.smear()
+        nu_energies = pd.playDarts(10000,NoCoreSys_dNdE.dNdE,c.NU_ENERGY_ARRAY)
+        htest = h.Event_Hist(nu_energies,c.NUMBINS,NoCoreSys_dNdE.Pos_Energy_Array[0], \
+                NoCoreSys_dNdE.Pos_Energy_Array[len(NoCoreSys_dNdE.Pos_Energy_Array) -1])
         splt.plot_EventHist(htest,oscParams[1],oscParams[0])
 
     if DEBUG == True:
         print("SHOWING SOME PLOTS OF dNdE's REBINNING")
         #First, build the untouched dNdE function
-        Perfect_dNdE = ns.build_Theory_dNdE(unosc_spectra,oscParams)
-        #Build the dNdE with US and/or CA core systematics included
         Varied_dNdE = ns.build_Theory_dNdE_wCoreSys(unosc_spectra, oscParams)
+        TotEvents = RoughIntegrate(Varied_dNdE.dNdE,Varied_dNdE.Nu_Energy_Array)
+        print("TOTEVENTS BEFORE SMEAR: " + str(TotEvents))
+        splt.dNdEPlot_line(Varied_dNdE.Pos_Energy_Array, \
+                Varied_dNdE.dNdE, oscParams[1], oscParams[0])
         if "DETECTOR_RESP" in c.SYSTEMATICS:
-            #FIXME: Don't want resolution hard-coded... 
-            Varied_dNdE.setResolution(0.075)
+            Varied_dNdE.setResolution(c.RESOLUTION)
             Varied_dNdE.smear()
-        RoughIntegrate(Varied_dNdE.dNdE,c.ENERGY_ARRAY)
-        splt.dNdEPlot_line(c.ENERGY_ARRAY,Varied_dNdE.dNdE, oscParams[1],\
+        TotEvents = RoughIntegrate(Varied_dNdE.dNdE,Varied_dNdE.Pos_Energy_Array)
+        print("TOTEVENTS AFTER SMEAR: " + str(TotEvents))
+        splt.dNdEPlot_line(Varied_dNdE.Pos_Energy_Array,Varied_dNdE.dNdE, oscParams[1],\
                 oscParams[0])
-        dNdEHistperf = h.dNdE_Hist(Perfect_dNdE,c.NUMBINS)
+        dNdEHistperf = h.dNdE_Hist(NoCoreSys_dNdE,c.NUMBINS)
         dNdEHistvar = h.dNdE_Hist(Varied_dNdE,c.NUMBINS)
-        print("HIST BINNING FROM n.dNdE_Hist")
-        print(dNdEHistvar.bin_lefts[0])
-        print(dNdEHistvar.bin_rights[len(dNdEHistvar.bin_rights)-1])
-        dNdEHistvar = pd.playDarts_h(188,dNdEHistvar)
+        dNdEHistvar = pd.playDarts_h(TotEvents,dNdEHistvar)
         splt.dNdEPlot_pts(dNdEHistperf.bin_centers,dNdEHistperf.bin_values, \
                 dNdEHistperf.bin_lefts,dNdEHistperf.bin_rights, \
                 oscParams[1], oscParams[0])